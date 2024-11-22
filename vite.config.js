4e908c476dba5a32444b65d9147afb5d9d08dd58
jodydev <jodyossino2001@gmail.com>
2024-11-22 22:12:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer'; 
+// import { visualizer } from 'rollup-plugin-visualizer'; 
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }) 
+    // visualizer({ open: true }) 
   ],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Gruppo più specifico per librerie come React, lodash, ecc.
-            if (id.includes('react')) {
-              return 'react-vendor';
-            }
-            if (id.includes('lodash')) {
-              return 'lodash-vendor';
-            }
-            return 'vendor'; // Gruppo generale per tutte le altre librerie
-          }
-        },
-      },
-    },
     chunkSizeWarningLimit: 4000,
   },
 });
